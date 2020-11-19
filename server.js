7d595e92a6d0112e1a442377b2404130bceda839
gkdlswm <andrewkimunr@gmail.com>
2020-11-19 14:07:30 -0800

    server setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+const MONGO_URI =
+  "mongodb+srv://test:qweeasdd123@cluster0.zfw9e.mongodb.net/boilerPlate?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log(`Connected to mongo DB`);
+    app.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}`);
+    });
+  });
