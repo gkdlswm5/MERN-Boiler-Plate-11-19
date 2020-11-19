d16e5c360aa343b8d0cbe75a490a804a62bb38f1
gkdlswm <andrewkimunr@gmail.com>
2020-11-19 14:40:00 -0800

    mongo added

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI =
   "mongodb+srv://test:qweeasdd123@cluster0.zfw9e.mongodb.net/boilerPlate?retryWrites=true&w=majority";
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
