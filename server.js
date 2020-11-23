1009cff5061c761b0f4ad3540880adcf2b215e72
gkdlswm <andrewkimunr@gmail.com>
2020-11-22 20:53:53 -0800

    server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-//
+//register route
 app.post("/register", async (req, res) => {
   try {
     const hashedPass = await bcrypt.hash;
